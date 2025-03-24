ef54086988fb15f91332612854c9db9ba3de3dca
willifin.github.io <yuliyas.pro@yandex.ru>
2025-03-25 01:01:03 +0300

    Code optim2

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const answer = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${answer}!`);
   return answer;
 };
 
-// export greeting default;
+export default greeting ;
