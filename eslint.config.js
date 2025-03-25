62c877acc989894a1357e91c237f3e8351721b08
willifin.github.io <yuliyas.pro@yandex.ru>
2025-03-26 02:19:31 +0300

    Code optim3

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended
+  recommendedConfig: pluginJs.configs.recommended,
 });
 
 export default [
